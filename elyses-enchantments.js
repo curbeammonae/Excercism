36ae866c12d2038e6b5535993d784b6ab147b66d
Monae Curbeam <curbeammonae@gmail.com>
2023-04-05 14:46:13 -0500

    newproblem

---
+++ elyses-enchantments.js
@@ -0,0 +1,11 @@
+// To pick a card, return the card at index position from the given stack.
+
+// const position = 2;
+// getItem([1, 2, 4, 1], position);
+// // => 4
+
+
+export function getItem(cards, position) {
+    return cards[position]
+ }
+ 
