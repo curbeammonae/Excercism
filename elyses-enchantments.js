4860f6f929005ee98bd7697428e9a0de372ef913
Monae Curbeam <curbeammonae@gmail.com>
2023-04-06 03:45:44 -0500

    splice

--- elyses-enchantments.js
+++ elyses-enchantments.js
@@ -9,3 +9,14 @@ export function getItem(cards, position) {
     return cards[position]
  }
  
+//  Perform some sleight of hand and exchange the card at index position with the replacement card provided. Return the adjusted stack.
+
+// const position = 2;
+// const replacementCard = 6;
+// setItem([1, 2, 4, 1], position, replacementCard);
+// // => [1, 2, 6, 1]
+
+export function setItem(cards, position, replacementCard) {
+    cards.splice(position, 1, replacementCard);
+     return cards;
+   }
