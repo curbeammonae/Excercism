340a570cf989d5747d0334b14b63ee0da80a8728
Monae Curbeam <curbeammonae@gmail.com>
2023-05-23 19:20:20 -0500

    objects

---
+++ high-score-board.js
@@ -0,0 +1,7 @@
+// Create a function createScoreBoard that creates an object that serves as a high score board. The keys of this object will be the names of the players, the values will be their scores. For testing purposes, you want to directly include one entry in the object. This initial entry should consist of The Best Ever as player name and 1000000 as score.
+function createScoreBoard() {
+    const obj = {
+      'The Best Ever': 1000000
+    }
+    return obj
+  }
