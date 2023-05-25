fb0eaba7691718c730148fd0bfd2742bf66599b8
Monae Curbeam <curbeammonae@gmail.com>
2023-05-24 22:01:30 -0500

    objects

--- high-score-board.js
+++ high-score-board.js
@@ -5,3 +5,17 @@ function createScoreBoard() {
     }
     return obj
   }
+
+//   To add a player to the high score board, define the function addPlayer. It accepts 3 parameters:
+
+// The first parameter is an existing score board object.
+// The second parameter is the name of a player as a string.
+// The third parameter is the score as a number.
+// The function returns the same score board object that was passed in after adding the new player.
+
+// addPlayer({ 'Dave Thomas': 0 }, 'José Valim', 486373);
+// // => {'Dave Thomas': 0, 'José Valim': 486373}
+
+function addPlayer(scoreBoard, player, score) {
+    return scoreBoard[player,score]
+}
