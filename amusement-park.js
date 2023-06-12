5dd41e77ab5dab5353c4bb863dd420cbfbfd5b63
Monae Curbeam <curbeammonae@gmail.com>
2023-06-12 08:46:25 -0500

    new answer

--- amusement-park.js
+++ amusement-park.js
@@ -37,3 +37,32 @@ function revokeTicket(visitor) {
     visitor.ticketId = null
     return visitor
   }
+
+//   To prevent forgery, the ticket identifiers are unique. Once a ticket is printed, its identifier is added as a key in an object in the system so it can be tracked.
+
+// Before the ticket is sold to a visitor, it is stored with the value null in the ticket tracking object. When it is sold to a visitor, the visitor's name is assigned as a value. When employees have doubts about the validity of a ticket, they need to check the status of the ticket in the system.
+
+// Implement a function ticketStatus that accepts the tracking object and a ticket identifier as arguments. It should return one of the following results.
+
+// 'unknown ticket id' if the identifier was not found in the tracking object
+// 'not sold' in case the ticket was printed but not sold
+// 'sold to {name}' where {name} is the name of the visitor if the ticket was sold
+// const tickets = {
+//   '0H2AZ123': null,
+//   '23LA9T41': 'Verena Nardi',
+// };
+
+// ticketStatus(tickets, 'RE90VAW7');
+// // => 'unknown ticket id'
+
+// ticketStatus(tickets, '0H2AZ123');
+// // => 'not sold'
+
+// ticketStatus(tickets, '23LA9T41');
+// // => 'sold to Verena Nardi'
+
+function ticketStatus(tickets, ticketId) {
+    if(!tickets.ticketId){
+        return null
+    }
+  }
