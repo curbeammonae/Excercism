c5b8c78372e0920d6b4e73f37263ff9787a8cdff
Monae Curbeam <curbeammonae@gmail.com>
2023-08-31 17:19:38 -0500

    answer

--- destructured-elements.js
+++ destructured-elements.js
@@ -9,3 +9,17 @@ function discardTopCard(deck) {
     const [first, ...everythingElse] = deck;
     return [first, everythingElse];
     }
+
+
+    // Elyse performs sleight of hand and summons the second card in the deck without using the array[index].
+
+    // const deck = [3, 2, 10, 6, 7];
+    
+    // getSecondCard(deck);
+    // //
+
+
+function getSecondCard(deck) {
+        const [first, second, ...everythingElse] = deck;
+        return second;
+        }
