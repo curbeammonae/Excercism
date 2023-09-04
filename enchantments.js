28172956dc48a199cd17f2baedc2db8e0e03fcb2
Monae Curbeam <curbeammonae@gmail.com>
2023-09-04 14:18:30 -0500

    answer

---
+++ enchantments.js
@@ -0,0 +1,9 @@
+// Elyse wants to know the position (index) of a card in the stack.
+
+// const card = 2;
+// getCardPosition([9, 7, 3, 2], card);
+// // => 3
+
+function getCardPosition(stack, card) {
+    return stack.indexOf(card)
+}  
