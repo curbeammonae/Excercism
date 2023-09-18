4c1d0569d9ec96da9ebdcd6ba3601f2f17a32277
Monae Curbeam <curbeammonae@gmail.com>
2023-09-18 01:26:48 -0500

    answer

--- transformative-enchantments.js
+++ transformative-enchantments.js
@@ -8,3 +8,18 @@
 function seeingDouble(deck) {
     return deck.map(card => 2 * card);
     }
+
+// Elyse wants to triplicate every 3 found in the deck. If a deck started with a single 3, after the trick the deck would have three 3's in place of the original.
+// const deck = [1, 3, 9, 3, 7];
+// threeOfEachThree(deck);
+//     // => [1, 3, 3, 3, 9, 3, 3, 3
+
+function threeOfEachThree(deck) {
+    return deck.reduce((result, card) => {
+    if (card == 3)
+    result.push(3, 3, 3);
+    else
+    result.push(card);
+    return result
+    }, []);
+    }
