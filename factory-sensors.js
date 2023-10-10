b12bd86fec375a42b1d84db193d31091eba4aded
Monae Curbeam <curbeammonae@gmail.com>
2023-10-10 11:30:43 -0500

    answer

--- factory-sensors.js
+++ factory-sensors.js
@@ -25,3 +25,21 @@ class OverheatingError extends Error {
     );
     }
     }
+
+    // Elena is very pleased with your first assignment and asks you to deal with the monitoring of the machines' temperature. While chatting with a technician, Greg, you are told that if the temperature of a machine exceeds 500°C, the technicians start worrying about overheating.
+
+    // The machine is equipped with a sensor that measures its internal temperature. You should know that the sensor is very sensitive and often breaks. In this case, the technicians will need to change it.
+    
+    // Your job is to implement a function reportOverheating that takes the temperature as a parameter and throws an error if the sensor is broken or if the machine starts overheating. Knowing that you will later need to react differently depending on the error, you need a mechanism to differentiate the two kinds of errors. You could rely on the error messages, but this would make your code fragile as it would break if the message gets updated. So to be able to do so properly, you'll throw instances of different error class
+
+    function reportOverheating(temperature) {
+        if (temperature === null) {
+        throw new ArgumentError(
+        'The sensor seems broken. Expected temperature, actual null.'
+        );
+        }
+        if (temperature < 500) {
+        return;
+        }
+        throw new OverheatingError(temperature);
+        }
