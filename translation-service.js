9082308dc3d64fb4bf095c83c98c7719442422ea
Monae Curbeam <curbeammonae@gmail.com>
2023-10-13 21:15:03 -0500

    answer

---
+++ translation-service.js
@@ -0,0 +1,7 @@
+// Implement a method free(text) that provides free members with translation that already exist in the API storage. Ignore the quality and forward any errors thrown by the API.
+
+// Returns the translation if it can be retrieved, regardless of its quality
+// Forwards any error from the translation API
+// Uses the api.fetch method (api.fetch returns a promise
+
+
